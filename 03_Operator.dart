993516cd353b6a110a5e788205d5745ff58a8cf7
mclenz2018 <41286288+mclenz2018@users.noreply.github.com>
2020-07-07 13:00:01 +0200

    Create 03_Operator.dart

---
+++ 03_Operator.dart
@@ -0,0 +1,11 @@
+main(){
+  int zahl = 0;
+  zahl = zahl + 99;
+  print ("0 plus 99 ist $zahl.");
+  zahl = zahl * 3;
+  print("99 mal 3 ist $zahl.");
+  zahl--;
+  print(zahl);
+  zahl*=zahl;
+  print(zahl);
+}
